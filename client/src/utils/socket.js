36bde2bffdf0a4b386e79fab7fc245caf2541d2f
Irshadudheen <146156401+Irshadudheen@users.noreply.github.com>
2025-07-02 20:01:14 +0530

    update to websocket

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,10 +1,10 @@
 import { io } from 'socket.io-client';
 
 const socket = io(import.meta.env.VITE_BACKEND_URL, {
-// Use WebSocket transport
-  reconnection: true,        // Enable automatic reconnection
-  reconnectionAttempts: 5,   // Number of reconnection attempts
-  reconnectionDelay: 1000,   // Delay between reconnection attempts
+  transports: ['websocket'],
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
 });
 
 export default socket;
