83c85039936b1d574525f321f1d74afd4ed9e766
Irshadudheen <146156401+Irshadudheen@users.noreply.github.com>
2025-07-07 16:26:48 +0530

    update live tag position once more

--- client/src/pages/Talkspace/talkspace.jsx
+++ client/src/pages/Talkspace/talkspace.jsx
@@ -266,7 +266,7 @@ export default function Talkspace() {
               </div>
             </div>
             {!isLoading && (
-              <div className="absolute -top-2 -right-0.5 sm:right-13 bg-yellow-400 text-xs text-yellow-900 px-2 py-1 rounded-full transform rotate-12 shadow-sm font-medium">
+              <div className="absolute -top-2  -right-13   bg-yellow-400 text-xs text-yellow-900 px-2 py-1 rounded-full transform rotate-12 shadow-sm font-medium">
                 {liveCount||0}+ online now
               </div>
             )}
